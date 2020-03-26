d6ca02b3970e72daa36839221ca6028e230e68ac
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-03-26 16:02:56 -0700

    first commit to initialize git again

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
-        BarkingDog.shouldWakeUp(true, 9);
-        LeapYear.isLeapYear(2020);
+        calculateScore("Tim", 900);
     }
 
     public static int calculateScore(String playerName, int score) {
-        System.out.println("Player " + playerName + " scored" + score + " points");
+        System.out.println("Player " + playerName + " scored " + score + " points");
         return score * 1000;
     }
 }
